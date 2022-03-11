57dc1367a00c745cf7d553e42be2be344a6b7e70
kmatulud <princessmakgotso@gmail.com>
2022-03-11 15:28:44 +0200

    yml file

--- fruitFactory.js
+++ fruitFactory.js
@@ -1,18 +1,47 @@
 const FruitFactoryFunction = () => {
-    const fruitNames = ["Grapes", "Melon", "Watermelon", "Tangerine", "Lemon", "Banana", "Pineapple", "Mango", "Red Apple"];
-    const fruits = ["🍇", "🍈", "🍉", "🍊", "🍋", "🍌", "🍍", "🥭", "🍎"];
-    // fruitNames.sort()
-    // fruits.sort()
+    let fruitNames = ["Grapes", "Melon", "Watermelon", "Tangerine", "Lemon", "Banana", "Pineapple", "Mango", "Red Apple"];
+    let fruits = ["🍇", "🍈", "🍉", "🍊", "🍋", "🍌", "🍍", "🥭", "🍎"];
 
     let fruitsAndEmos = [];
     const displayFruitWithEmo = () => {
         fruitsAndEmos = fruitNames.map((fruit)=>{
-            let getFruitIndex = fruitNames.indexOf(fruit)
+            let getFruitIndex = fruitNames.indexOf(fruit);
             return fruit + fruits[getFruitIndex];
         })
-        return fruitsAndEmos;
+        return fruitsAndEmos.sort();
+    }    
+    
+    const addFruitEmo = (fruit, emo) => {
+        if (fruit != "" && emo != "") {
+            //inserting values into the designated arrays, if boxes are not empty and displaying a success message.
+            fruitNames.push(fruit);
+            fruits.push(emo);
+            classList.add("show");
+            setTimeout(() => {
+                // hides element 
+               classList.remove("show");
+            }, 5000);
+        } else {
+            err.classList.add("show");
+            setTimeout(() => {
+                // hides element 
+                classList.remove("show");
+            }, 5000);
+        }
+      
     }
+
+ 
+const searchFruit = (searchItem) => {
+    const match = fruitsAndEmos.find(el => el.toUpperCase().indexOf(searchItem) > -1)
+    return match;
+}
+
     return{
         displayFruitWithEmo,
+        addFruitEmo,
+        searchFruit
     }
+
+
 }
--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 let addFruitElem = document.querySelector("#add-fruits");
 let addEmoElem = document.querySelector("#add-emojis");
 let submitElem = document.querySelector(".submitBtn");
+let searchbox = document.querySelector("#search");
+const searchIcon = document.querySelector(".fa-magnifying-glass");
+
+let fruitNames = ["Grapes", "Melon", "Watermelon", "Tangerine", "Lemon", "Banana", "Pineapple", "Mango", "Red Apple"];
+let fruits = ["🍇", "🍈", "🍉", "🍊", "🍋", "🍌", "🍍", "🥭", "🍎"];
 
 const fruitFactoryFunction = FruitFactoryFunction();
 
-// Fuction that creates a combined map
+const addFruits = () => {
+    //Getting values from HTML input boxes
+    const addFruit = addFruitElem.value;
+    const addEmoji = addEmoElem.value;
+    const err = document.querySelector(".fail");
+    const pass = document.querySelector(".success");
+
+    if (addFruit != "" && addEmoji != "") {
+        //inserting values into the designated arrays, if boxes are not empty and displaying a success message.
+        fruitNames.push(addFruit);
+        fruits.push(addEmoji);
+        pass.classList.add("show");
+        setTimeout(() => {
+            // hides element 
+            pass.classList.remove("show");
+        }, 5000);
+    } else {
+        err.classList.add("show");
+        setTimeout(() => {
+            // hides element 
+            err.classList.remove("show");
+        }, 5000);
+    }
+
+}
+
+// addFruits();
+
 const displayFruits = () => {
     let fruitNames = fruitFactoryFunction.displayFruitWithEmo();
     localStorage.setItem("fruitNames", JSON.stringify(fruitNames));
@@ -17,9 +49,7 @@ const viewFruits = () => {
     const newFruits = JSON.parse(localStorage.getItem("fruitNames"));
     const list = document.querySelector(".list");
 
-     for (let x = 0; x < newFruits.length; x++) {
-    console.log(newFruits[x]);
-
+    for (let x = 0; x < newFruits.length; x++) {
     const listItem = document.createElement("li");
     const listValue = document.createTextNode(newFruits[x]);
     listItem.appendChild(listValue);
@@ -31,20 +61,27 @@ const viewFruits = () => {
  viewFruits();
 
 
-const addFruits = () => {
-    let addFruitElems = addFruitElem.value
-    let addEmoElems = addFruitElem.value
 
-    fruitNames.push(addFruitElems);
-    fruits.push(addEmoElems);
-    return false;
-}
-function findFruit(){
+function searchFruits (){
+    // Declare variables
+    const filter = searchbox.value.toUpperCase(); //Turns everything to uppercase, to avoid case sensitivity
+    const ul = document.querySelector(".list"); //List parent
+    const li = ul.getElementsByTagName("li"); //List Items (child of UL)
 
-}
-// console.log(fruitNames);
-// window.addEventListener("load", displayFruits);
-if(submitElem){
-    submitElem.addEventListener('click', addFruits, false);
+  // Loop through all list items, and hide those who don't match the search query
+  for ( let i = 0; i < li.length; i++) {
+    const item = li[i];
+    const txtValue = item.textContent || item.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
   }
-// submitElem.addEventListener("click", addFruits);
+}
+
+// searchFruits();
+
+submitElem.addEventListener("click", addFruits);
+// searchIcon.addEventListener("click", searchFruits);
+
